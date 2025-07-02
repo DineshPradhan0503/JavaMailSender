40ba8131f483020b05a3f39604c9f7f7746fa57a
Dinesh <dineshpradhan0305@gmail.com>
2025-07-02 15:57:38 +0530

    latest

--- email-sender/src/main/java/com/bluepal/controller/EmailController.java
+++ email-sender/src/main/java/com/bluepal/controller/EmailController.java
@@ -1,6 +1,5 @@
 package com.bluepal.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,32 +11,28 @@ import com.bluepal.service.EmailService;
 @RequestMapping("/api/email")
 public class EmailController {
 
-    @Autowired
-    private EmailService emailService;
+	@Autowired
+	private EmailService emailService;
 
-    @PostMapping("/send")
-    public String sendEmail(
-        @RequestParam String to,
-        @RequestParam String subject,
-        @RequestParam String message,
-        @RequestParam(defaultValue = "false") boolean html,
-        @RequestParam(required = false) MultipartFile attachment,
-        @RequestParam(required = false) MultipartFile inlineImage
-    ) {
-        try {
-            EmailRequest email = new EmailRequest();
-            email.to = to;
-            email.subject = subject;
-            email.message = message;
-            email.html = html;
-            email.attachment = attachment;
-            email.inlineImage = inlineImage;
+	@PostMapping("/send")
+	public String sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String message,
+			@RequestParam(defaultValue = "false") boolean html,
+			@RequestParam(required = false) MultipartFile attachment,
+			@RequestParam(required = false) MultipartFile inlineImage) {
+		try {
+			EmailRequest email = new EmailRequest();
+			email.to = to;
+			email.subject = subject;
+			email.message = message;
+			email.html = html;
+			email.attachment = attachment;
+			email.inlineImage = inlineImage;
 
-            emailService.sendEmail(email);
-            return "Email sent successfully!";
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Failed to send email: " + e.getMessage();
-        }
-    }
+			emailService.sendEmail(email);
+			return "Email sent successfully!";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Failed to send email: " + e.getMessage();
+		}
+	}
 }
--- email-sender/src/main/java/com/bluepal/dto/EmailRequest.java
+++ email-sender/src/main/java/com/bluepal/dto/EmailRequest.java
@@ -4,9 +4,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class EmailRequest {
 	public String to;
-    public String subject;
-    public String message;
-    public boolean html;
-    public MultipartFile attachment;
-    public MultipartFile inlineImage;
+	public String subject;
+	public String message;
+	public boolean html;
+	public MultipartFile attachment;
+	public MultipartFile inlineImage;
 }
